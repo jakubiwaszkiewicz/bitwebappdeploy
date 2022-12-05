f7273e5918a1e27df436783ae5a708f1781bfa18
jakubiwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2022-12-05 20:46:57 +0100

    adasda

--- src/Components/PartnerzyPage.js
+++ src/Components/PartnerzyPage.js
@@ -1,40 +1,33 @@
 import "./styles/partnerzy.css"
-import LOGO1 from "../pictures/logo1.png"
+import logoCDAction from "../pictures/logos/cd-action.webp"
+import logoDolnySlask from "../pictures/logos/dolny-slask.webp"
+import logoPCFormat from "../pictures/logos/pc-format.webp"
+import logoPWR from "../pictures/logos/pwr.webp"
+import logoRadioLuz from "../pictures/logos/radio-luz.webp"
+import logoTVSStyk from "../pictures/logos/tvs-styk.webp"
+import logoUnit8 from "../pictures/logos/unit8.webp"
 
 export default function PartnerzyPage () {
   return (
     <section id="partnerzy" className="appear">
-      <h3>Partnerzy</h3>
-        <div className="partnerzy--boxes ">
-            <h3 className="wkrotce">Już wkrótce!</h3>
-            {/*<h3>Partner dużo kaski</h3>
-            <div className="partnerzy--box">
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-            </div>
-            <h3>Partner średnio kaski</h3>
-            <div className="partnerzy--box">
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-            </div>
-            <h3>Partner mało kaski</h3>
-            <div className="partnerzy--box">
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-                <img src={LOGO1} alt="cos tam" />
-            </div>*/}
+        <div className="partnerzy--boxes">
             {/*<h3 className="wkrotce">Już wkrótce!</h3>*/}
+            <h3>Partner</h3>
+            <div className="partnerzy--box">
+                <a href="https://unit8.com/"><img className="logo-big" src={logoUnit8} alt="Logo Unit8" /></a>
+            </div>
+            <h3>Patronaty honorowe</h3>
+            <div className="partnerzy--box">
+                <img src={logoPWR} alt="Logo Politechniki Wrocławskiej" />
+                <img src={logoDolnySlask} alt="Logo Marszałka Wojewódzkiego" />
+            </div>
+            <h3>Partronaty medialne</h3>
+            <div className="partnerzy--box">
+                <img src={logoCDAction} alt="cos tam" />
+                <img src={logoTVSStyk} alt="cos tam" />
+                <img src={logoPCFormat} alt="cos tam" />
+                <img src={logoRadioLuz} alt="cos tam" />
+            </div>
         </div>
     </section>
   )
